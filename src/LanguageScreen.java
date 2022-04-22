d3f5489f54bdecefed9d3fcc4c6d27ea9dfdaef7
Chris <chrissofroniou1@gmail.com>
2022-04-22 17:13:03 +0100

    Language Screen Panels and Select Screen.

---
+++ src/LanguageScreen.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.Color;
+
+public class LanguageScreen  {
+    private JButton portugeseButton;
+    private JButton italianButton;
+    private JButton russianButton;
+    private JButton frenchButton;
+    private JButton spanishButton;
+    private JButton germanButton;
+
+    JPanel jp = new JPanel();
+
+
+
+    public LanguageScreen(){
+        jp.setBackground(Color.BLUE);
+
+        jp.setSize(700,600);
+    }
+
+
+
+}
---
+++ src/SelectScreen.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class SelectScreen {
+    private JButton teacherButton;
+    private JButton studentButton;
+
+}
